bc02f8b79461cb2791da6d9622337799d9cf5e74
Scott Spence <scottspence10@gmail.com>
2017-03-14 14:03:43 +0000

    Webpack, React, and Hot Module Replacement

--- src/shared/config.js
+++ src/shared/config.js
@@ -3,3 +3,8 @@
 export const WEB_PORT = process.env.PORT || 8080
 export const STATIC_PATH = '/static'
 export const APP_NAME = 'Just another hello world app!'
+
+export const WDS_PORT = 8081
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
