90321d5f18c6af3e8b445c52409667873d727d3c
Scott Spence <scottspence10@gmail.com>
2017-03-14 11:11:30 +0000

    Express, Nodemon, and PM2

--- src/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-
-const Dog = require('./dog')
-
-const toby = new Dog('Toby')
-
-console.log(toby.bark())
---
+++ src/shared/config.js
@@ -0,0 +1,5 @@
+// @flow
+
+export const WEB_PORT = process.env.PORT || 8080
+export const STATIC_PATH = '/static'
+export const APP_NAME = 'Just another hello world app!'
