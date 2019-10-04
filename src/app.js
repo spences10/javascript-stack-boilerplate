fc6b77f5c8baf74671cda8e26419766edb188ab5
Scott Spence <scott.spence@karmarama.com>
2019-10-04 14:36:24 +0100

    plugin-proposal-class-properties

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 
 export default () => {
   const [count, setCount] = useState(0)
+  state = {}
   return (
     <>
       <h1>Hello world!</h1>
       <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
-      <p>Counter is now {count} counts of counting!</p>
+      <h2>Counter is now {count} counts of counting!</h2>
     </>
   )
 }
