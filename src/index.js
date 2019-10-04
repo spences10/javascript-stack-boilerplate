a477ddc3add0bc303de8c9f5f23f213f3b6e1aee
Scott Spence <scott.spence@karmarama.com>
2019-10-04 13:42:56 +0100

    config html template and babel react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import getGreeting from './greet'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
 
-console.log(getGreeting('world!'))
+ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
-const path = require('path')
+const path = require(`path`)
+const HtmlWebpackPlugin = require(`html-webpack-plugin`)
 
 module.exports = {
-  mode: 'production',
+  mode: `production`,
   entry: `./src/index.js`,
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.bundle.js',
+    path: path.join(__dirname, `dist`),
+    filename: `app.bundle.js`,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: `babel-loader`,
         exclude: /node_modules/,
         options: {
-          presets: ['@babel/preset-env'],
+          presets: [`@babel/preset-env`, `@babel/preset-react`],
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./src/index.html`,
+    }),
+  ],
 }
