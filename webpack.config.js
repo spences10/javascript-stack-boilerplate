82bca8fdb7236a39ec0a340bc0e7e20dbc20dff0
Scott Spence <scott.spence@karmarama.com>
2019-10-04 12:44:08 +0100

    configure babel 🤯

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,16 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'app.bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
 }
