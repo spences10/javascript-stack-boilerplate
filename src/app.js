fd3c3c8303a176075d4548de09ba3afdd32b27d3
Scott Spence <scott.spence@karmarama.com>
2019-10-06 21:26:03 +0100

    hot dev👩‍🚒

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ export default () => {
   const [count, setCount] = useState(0)
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1>Hello world!!</h1>
       <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
       <h2>Counter is now {count} counts of counting!</h2>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const render = App => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('app')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    render(App)
+  })
+}
