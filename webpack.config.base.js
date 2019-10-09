e526ede6f4c10f4c3f60b407b6088154e9020b40
Scott Spence <scott.spence@karmarama.com>
2019-10-09 17:12:55 +0100

    add prettier and pretty quick with husky

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,51 +1,29 @@
-const path = require(`path`)
-const HtmlWebpackPlugin = require(`html-webpack-plugin`)
+const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
 
 module.exports = {
   entry: `./src/index.js`,
   output: {
     path: path.join(__dirname, `dist`),
-    filename: `app.bundle.js`,
+    filename: `app.bundle.js`
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: `babel-loader`,
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: [
-                  'last 2 versions',
-                  'not dead',
-                  'not < 2%',
-                  'not ie 11',
-                ],
-                useBuiltIns: 'entry',
-              },
-            ],
-            '@babel/preset-react',
-          ],
-          plugins: [
-            `react-hot-loader/babel`,
-            `@babel/plugin-proposal-class-properties`,
-            `@babel/plugin-syntax-dynamic-import`,
-          ],
-        },
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
         use: [`style-loader`, `css-loader`],
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: `./src/index.html`,
-    }),
-  ],
-}
+      template: `./src/index.html`
+    })
+  ]
+};
